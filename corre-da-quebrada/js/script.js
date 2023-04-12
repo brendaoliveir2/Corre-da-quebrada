4fa0b16d48c7aa6c93ddac0d1bc0b0a0bd5bf1d1
Brenda Gabriele De Oliveira Luz <126905373+brendaoliveir2@users.noreply.github.com>
2023-04-12 00:42:17 -0300

    ok

---
+++ corre-da-quebrada/js/script.js
@@ -0,0 +1,29 @@
+let navbar = document.querySelector('.navbar'); // menu header
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form'); // pesquisa
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container'); // carrinho
+
+document.querySelector('#carrinho-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+} 
